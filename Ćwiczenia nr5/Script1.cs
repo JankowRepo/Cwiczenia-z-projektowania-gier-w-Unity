5c84244b279b183c75c846a3d83bcf3847b2227e
Jankorepo <44462192+Jankorepo@users.noreply.github.com>
2021-11-14 10:27:31 +0100

    Zadania z ćwiczenia nr 5 oraz zadanie dodatkowe

---
+++ Ćwiczenia nr5/Script1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour
+{
+    public float platformSpeed = 3f;
+    private bool isRunning = false;
+    public float distance = 6.6f;
+    private float leftPos;
+    private float rightPos;
+
+    void Start()
+    {
+        rightPos = transform.position.x + distance;
+        leftPos = transform.position.x;
+    }
+
+    void FixedUpdate()
+    {
+        
+        if (transform.position.x < leftPos && platformSpeed<0)
+        {
+            isRunning = false;
+        }
+        else if (transform.position.x >= rightPos)
+        {
+            platformSpeed = -platformSpeed;
+        }
+        if (isRunning)
+        {
+            Vector3 move = transform.right * platformSpeed * Time.deltaTime;
+            transform.Translate(move);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.parent = transform;
+            platformSpeed = Mathf.Abs(platformSpeed);
+            isRunning = true;
+        }
+    }
+}
---
+++ Ćwiczenia nr5/Script2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    public float platformSpeed = 1.5f;
+    private bool isRunning = false;
+    public float distance = 2f;
+    private float leftPos;
+    private float rightPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightPos = transform.position.z + distance;
+        leftPos = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < leftPos && platformSpeed < 0)
+        {
+            isRunning = false;
+        }
+        else if (transform.position.z >= rightPos)
+        {
+            platformSpeed = -platformSpeed;
+        }
+        if (isRunning)
+        {
+            Vector3 move = -transform.forward * platformSpeed * Time.deltaTime;
+            transform.Translate(move);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            platformSpeed = Mathf.Abs(platformSpeed);
+            isRunning = true;
+        }
+    }
+}
